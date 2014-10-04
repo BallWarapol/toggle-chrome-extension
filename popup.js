dc200eb2965a9cafe4ce18a564156b4b12bb6efe
Ball Warapol <xxx123xcv@gmail.com>
2014-10-04 12:05:03 +0700

    Move window.close into getAll

--- popup.js
+++ popup.js
@@ -2,8 +2,28 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-
 function click(e) {
+		chrome.management.getAll(function(extensionInfo){
+			alert(extensionInfo)
+			for(var n in extensionInfo){
+				alert(extensionInfo[n].id+"--"+e.target.id)
+				if (extensionInfo[n].id+"" == e.target.id+"") {
+					alert(extensionInfo[n].id+"--"+e.target.id)
+					if(extensionInfo[n].enabled == true &&
+					extensionInfo[n].isApp != true &&
+					extensionInfo[n].mayDisable == true ){
+						chrome.management.setEnabled(extensionInfo[n].id, false);
+					} else {chrome.management.setEnabled(extensionInfo[n].id, true);}
+				}
+			}
+			window.close();
+		});
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var divs = document.querySelectorAll('div');
+  for (var i = 0; i < divs.length; i++) {
+	divs[i].getAll=function () {
 		chrome.management.getAll(function(extensionInfo){
 			alert(extensionInfo)
 			for(var n in extensionInfo){
@@ -21,10 +41,6 @@ function click(e) {
 		});
   		window.close();
 }
-
-document.addEventListener('DOMContentLoaded', function () {
-  var divs = document.querySelectorAll('div');
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', click);
+    divs[i].addEventListener('click', divs[i].getAll);
   }
 });
